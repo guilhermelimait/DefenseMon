185fe46ddad2c52538d4e3314b5ba35316537548
Gui Lima <guilhermelimait@gmail.com>
2025-07-14 11:58:59 +0200

    Delete frontend directory

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class", // Always use dark mode
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
